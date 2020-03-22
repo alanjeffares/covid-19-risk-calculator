19cfa2a3a076f8bd171ffd4b5d13bcf0d5a0568b
TechyBiologist <nialldelahunty1@gmail.com>
2020-03-22 14:02:13 +0000

    Added request methods for dealing with input

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
-from flask import Flask,render_template, url_for, flash, redirect, jsonify
+from flask import Flask,render_template, url_for, request
 
 app=Flask(__name__)
 
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods=["GET","POST"])
 def home():
-    return render_template("home.html")
+    # Activity form
+    activity, duration = None, None
+    if request.method == "POST":
+        activity = request.form["activity"]
+        duration = request.form["duration"]
+    # Inputted values will be outputted to the screen when submit button is clicked [test]
+    return render_template("home.html", activity=activity, duration=duration)
 
 
 if __name__ == "__main__":
