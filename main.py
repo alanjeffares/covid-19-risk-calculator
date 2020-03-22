1b4b889fa7e5a97a5a74bbd9d8ce82ac304b6a69
alanjefares <alan.jeffares.1@ucdconnect.ie>
2020-03-22 22:15:31 +0000

    added responses

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
+import numpy as np
 import utils
 
 class User:
     def __init__(self):
-        self.age = None
-        self.sex = None
-        self.medical_risks = None
-        self.symptoms = None
-        self.local_risk = None
+        self.attributes = None
         self.city_infection_rate = 0.41
 
     def parse_responses(self, qa_list: list):
         """Extract responses and set them as class attributes"""
         [setattr(self, qa['attribute_name'], qa['response']) for qa in qa_list]
+        self.attributes = [qa['attribute_name'] for qa in qa_list]
 
     def calculate_risk(self):
+        """Calculate accumulated risk based on question responses"""
+        risk_scores = utils.json_loader('risk_scores.json')
+        risk_score_list = [risk_scores[attribute][getattr(self, attribute)]
+                           for attribute in self.attributes]
+        self.risk = np.prod(risk_score_list)*self.city_infection_rate
 
+
+class Decision:
+    def __init__(self):
+        pass
+
+    def parse_responses(self, qa_list: list):
+        """Extract responses and set them as class attributes"""
+        [setattr(self, qa['attribute_name'], qa['response']) for qa in qa_list]
+        self.attributes = [qa['attribute_name'] for qa in qa_list]
+
+    def evaluate_responses(self):
+        """Store feedback to user based on decisions"""
+        decision_responses = utils.json_loader('decision_responses.json')
+        self.response_list = [decision_responses[attribute][getattr(self, attribute)]
+                              for attribute in self.attributes]
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template, url_for, request
 import utils
+import main
 
 app=Flask(__name__)
 
@@ -21,7 +22,7 @@ def home():
         # creates list of questions to populate the page with
         test_dict[f'question_{id_}'] = decision_question_list[idx]
 
-# When submit is clicked
+    # When submit is clicked
     if request.method == "POST":
         
         # loop through questions and store respective answers
@@ -30,7 +31,7 @@ def home():
             try:
                 user_question_list[idx]['response'] = request.form[f'response_{id_}']
             except:
-                pass
+                raise ValueError
 
         for idx in range(len(decision_question_list)):
             id_ = decision_question_list[idx]['id']
@@ -39,9 +40,17 @@ def home():
             except:
                 pass
 
+        user = main.User()
+        user.parse_responses(user_question_list)
+        user.calculate_risk()
+        decision = main.Decision()
+        decision.parse_responses(decision_question_list)
+        decision.evaluate_responses()
+        print(user.risk, decision.response_list)
 
     # Inputted values will be outputted to the screen when submit button is clicked [test]
     return render_template("home.html", content=test_dict)
+
 
 
 if __name__ == "__main__":
