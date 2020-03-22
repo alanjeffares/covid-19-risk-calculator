444ebcb76f73d111cd78bf6d4717fa60c949acd7
TechyBiologist <nialldelahunty1@gmail.com>
2020-03-22 19:04:54 +0000

    retrieves the correct form info back (ex chkbox)

--- run.py
+++ run.py
@@ -6,17 +6,29 @@ app=Flask(__name__)
 @app.route("/", methods=["GET","POST"])
 def home():
     # Activity form
-    kwargs = {}
+    test_dict = {}
+    ans_list = []
+    tester =[]
     question_list = utils.json_loader('questions.json')
+
+    for idx in range(len(question_list)):
+        id_ = question_list[idx]['id']
+        # creates list of questions to populate the page with
+        test_dict[f'question_{id_}'] = question_list[idx]
+
+# When submit is clicked
     if request.method == "POST":
+        
         # loop through questions and store respective answers
         for idx in range(len(question_list)):
             id_ = question_list[idx]['id']
-            question_list[idx]['response'] = request.form[f'response_{id_}']
-            kwargs[f'question_{id_}'] = question_list[idx]
+            try: 
+                question_list[idx]['response'] = request.form[f'response_{id_}']
+            except:
+                pass
 
     # Inputted values will be outputted to the screen when submit button is clicked [test]
-    return render_template("home.html", **kwargs)
+    return render_template("home.html", content=test_dict)
 
 
 if __name__ == "__main__":
